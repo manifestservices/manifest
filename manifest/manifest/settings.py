2b3e419053bd6da48605b4c625d2aba7905dab71
thilakdesing <thilakdesing@yahoo.co.in>
2020-05-13 21:36:17 +0530

    settings

--- manifest/manifest/settings.py
+++ manifest/manifest/settings.py
@@ -88,6 +88,13 @@ WSGI_APPLICATION = 'manifest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
